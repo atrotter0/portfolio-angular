68363f6fd32175aa48e8dff3c6823e20870dcd5e
Abel Trotter <a_trotter0@hotmail.com>
2018-08-26 21:24:44 -0700

    Add loginService to component, add runLogin and runLogout methods

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,15 +1,37 @@
 import { Component, OnInit } from '@angular/core';
+import { LoginService } from './../services/login.service';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  styleUrls: ['./login.component.css'],
+  providers: [LoginService]
 })
 export class LoginComponent implements OnInit {
+  user;
+  private isLoggedIn: Boolean;
+  private userName: String;
 
-  constructor() { }
+  constructor(public loginService: LoginService) {
+    this.loginService.user.subscribe(user => {
+      if (user == null) {
+        this.isLoggedIn = false;
+      } else {
+        this.isLoggedIn = true;
+        this.userName = user.displayName;
+        console.log(user);
+      }
+    });
+  }
 
   ngOnInit() {
   }
 
+  runLogin(email: string, password: string) {
+    this.loginService.login(email, password);
+  }
+
+  runLogout() {
+    this.loginService.logout();
+  }
 }
